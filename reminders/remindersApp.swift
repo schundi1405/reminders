260c27f5163129e4c8950ee4ab185a5ac6ca1e59
schundi1405 <schundi1405@gmail.com>
2026-03-02 23:18:28 -0500

    Initial Commit

---
+++ reminders/remindersApp.swift
@@ -0,0 +1,17 @@
+//
+//  remindersApp.swift
+//  reminders
+//
+//  Created by Sindhu's iPad on 3/2/26.
+//
+
+import SwiftUI
+
+@main
+struct remindersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
